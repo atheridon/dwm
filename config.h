4bcc6edec8a95ce51b3e6d7b8ff4c0632fa5144e
atheridon <atheridon@gmail.com>
2020-07-28 01:23:38 +0200

    removed alpha patch and added systray patch

--- config.h
+++ config.h
@@ -3,6 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selecte    d monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display syst    ray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     	/* 0 means no systray */
 static const unsigned int gappih    = 25;	/* horiz inner gap between windows */
 static const unsigned int gappiv    = 25;	/* vert inner gap between windows */
 static const unsigned int gappoh    = 25;	/* horiz outer gap between windows and screen edge */
@@ -11,23 +15,16 @@ static const int smartgaps	    = 0;	/* 1 means no outer gap when there is only o
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "IBMPlexMono:size=13" };
+static const char dmenufont[]       = "IBMPlexMono:size=13";
 static const char col_black[]	    = "#000000";
 static const char col_gray[]        = "#444444";
 static const char col_white[]	    = "#ffffff";
 static const char col_yellow[]	    = "#ffff00";
-static const unsigned int baralpha  = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_white, col_black, col_gray },
 	[SchemeSel]  = { col_black, col_yellow,  col_yellow  },
-};
-static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
@@ -75,15 +72,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
-static const char *upvol[] = {"/usr/bin/amixer", "-q", "set", "Master", "5%+", "unm    ute", NULL};
-static const char *downvol[] = {"/usr/bin/amixer", "-q", "set", "Master", "5%-", "u    nmute", NULL};
-static const char *mutevol[] = {"/usr/bin/amixer", "-q", "set", "Master", "toggle",     NULL};
-static const char *brightup[] = {"/usr/bin/xbacklight", "-inc", "3", NULL};
-static const char *birghtdown[] = {"/usr/bin/xbacklight", "-dec", "3", NULL};
+static const char *upvol[] = {"/usr/bin/amixer", "-q", "set", "Master", "5%+", "unmute", NULL};
+static const char *downvol[] = {"/usr/bin/amixer", "-q", "set", "Master", "5%-", "unmute", NULL};
+static const char *mutevol[] = {"/usr/bin/amixer", "-q", "set", "Master", "toggle", NULL};
+static const char *brightup[] = {"/usr/bin/xbacklight", "-inc", "2", NULL};
+static const char *birghtdown[] = {"/usr/bin/xbacklight", "-dec", "2", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -126,12 +123,12 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,		XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,             		XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,			XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,             		XK_f,      togglefullscr,  {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
