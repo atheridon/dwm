9f72d2ffa60495c4fe7be75acf8a6230aeec9175
atheridon <atheridon@gmail.com>
2020-07-31 12:27:01 +0200

    added keybind

--- config.def.h
+++ config.def.h
@@ -116,10 +116,12 @@ static Key keys[] = {
         { 0,                    	XF86XK_MonBrightnessUp,    spawn,  SHCMD("/usr/bin/xbacklight -inc 2; pkill -RTMIN+2 dwmblocks") },
         { 0,                    	XF86XK_MonBrightnessDown,  spawn,  SHCMD("/usr/bin/xbacklight -dec 2; pkill -RTMIN+2 dwmblocks") },
 	{ MODKEY|ShiftMask,		XK_e,	   spawn,	   SHCMD("~/owncloud/Linux/scripts/pdmenu") },
+	{ MODKEY|ShiftMask,		XK_p,	   spawn,	   SHCMD("~/owncloud/Linux/scripts/xdisplay") },
 	{ MODKEY|ControlMask,		XK_l,	   spawn,	   SHCMD("~/owncloud/Linux/scripts/lock") },
 	{ MODKEY,			XK_e,	   spawn,	   SHCMD("st -e ranger") },
 	{ 0,				XK_Print,  spawn,	   SHCMD("scrot -q 100 ~/tmp/%b%d::%H%M%S.png && notify-send 'Screenshot taken!' && sxiv -t ~/tmp/") },
 	{ ShiftMask,			XK_Print,  spawn,	   SHCMD("sleep 0.2; scrot -q 100 -s ~/tmp/%b%d::%H%M%S.png && notify-send 'Screenshot taken!' && sxiv -t 9999 ~/tmp/") },
+
 	{ MODKEY,                       XK_numbersign,  togglescratch,     {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd } },
