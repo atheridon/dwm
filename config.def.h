e824dcc01f2a4c9057d0f34373562599912959a9
atheridon <atheridon@gmail.com>
2020-08-02 21:13:55 +0200

    changed dmenu style

--- config.def.h
+++ config.def.h
@@ -100,7 +100,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-g", "3", "-l", "5", "-fn", dmenufont, "-nb", dmenubg, "-p", dmenuprompt, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-g", "3", "-l", "10", "-fn", dmenufont, "-nb", dmenubg, "-p", dmenuprompt, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
