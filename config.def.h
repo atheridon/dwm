847883faea908488f05993d13f96eb39be4e6d9c
atheridon <atheridon@gmail.com>
2020-09-30 19:06:11 +0200

    personalization

--- config.def.h
+++ config.def.h
@@ -54,6 +54,7 @@ static const Rule rules[] = {
 	{ "microsoft teams - preview", NULL, NULL, 0,  	  1,          0,           0,        -1 },
 	{ "Gimp",    NULL,     NULL,           0,         0,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "PacketTracer7", "PacketTracer7",    "PacketTracer7",       0,    	   1,         0,          -1,        -1 },
 	{ "st-256color", NULL, NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
