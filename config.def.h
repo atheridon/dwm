1fcbe9b223bc3eb1849872f3eb7c9094db94cee7
atheridon <atheridon@gmail.com>
2020-10-14 15:16:47 +0200

    nothing changed

--- config.def.h
+++ config.def.h
@@ -109,7 +109,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-c", "-g", "3", "-l", "10", "-fn", dmenufont, "-nb", dmenubg, "-p", dmenuprompt, NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", dmenubg, "-p", dmenuprompt, NULL };
-static const char *termcmd[]  = { "st -e tmux", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "90x25", NULL };
 
